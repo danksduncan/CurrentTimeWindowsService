ffa4db81207c43a739f267d837822d487d60f65e
danksduncan <danksduncan@gmail.com>
2019-12-23 23:09:05 -0500

    Installer and service funtional. No longer in debug mode.

--- Service1.cs
+++ Service1.cs
@@ -20,56 +20,69 @@ namespace CurrentTimeWindowsService
             InitializeComponent();
         }
 
-        public void OnDeBug()
-        {
-            OnStart(null);
-        }
-
         private static System.Timers.Timer aTimer;
+        private static System.Timers.Timer bTimer;
         protected override void OnStart(string[] args)
         {
             aTimer = new System.Timers.Timer(5000);
             aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
             aTimer.Enabled = true;
+
+            bTimer = new System.Timers.Timer(600000);
+            bTimer.Elapsed += new ElapsedEventHandler(OnDeleteEvent);
+            bTimer.Enabled = true;
+        }
+
+        private static void OnDeleteEvent(object source, ElapsedEventArgs e)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "CurrentTime.txt";
+
+            if (!File.Exists(path))
+            {
+
+            }
+            else if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
         }
 
         private static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
-            string path = AppDomain.CurrentDomain.BaseDirectory + "CurrentTime.txt";
             string currentDateAndTime = DateTime.Now.ToString("h:mm:ss tt");
+            string filePath = (AppDomain.CurrentDomain.BaseDirectory + "CurrentTime.txt");
 
-            if (!File.Exists(path))
+            using (StreamWriter sw = new StreamWriter(filePath, true))
             {
-                File.Create(path);
-                TextWriter tw = new StreamWriter(path);
-                tw.WriteLine("The current time in Orlando is: " + currentDateAndTime);
-                tw.Close();
-            }
-            else if (File.Exists(path))
-            {
-                TextWriter tw = new StreamWriter(path, true);
-                tw.WriteLine("The current time in Orlando is: " + currentDateAndTime);
-                tw.Close();
+                if (!File.Exists(filePath))
+                {
+                    File.Create(filePath);
+                    sw.WriteLine("The current time in Orlando is: " + currentDateAndTime);
+                }
+                else if (File.Exists(filePath))
+                {
+                    sw.WriteLine("The current time in Orlando is: " + currentDateAndTime);
+                }
+                sw.Close();
             }
         }
 
         protected override void OnStop()
         {
-            string endServicePath = AppDomain.CurrentDomain.BaseDirectory + "ServiceEnded.txt";
-            string endServiceDateAndTime = DateTime.Now.ToString("h:mm:ss tt");
+            string endTime = DateTime.Now.ToString("h:mm:ss tt");
+            string filePath = (AppDomain.CurrentDomain.BaseDirectory + "ServiceEnded.txt");
 
-            if (!File.Exists(endServicePath))
+            using (StreamWriter endWriter = new StreamWriter(filePath, true))
             {
-                File.Create(endServicePath);
-                TextWriter tw = new StreamWriter(endServicePath);
-                tw.WriteLine("Service ended at " + endServiceDateAndTime);
-                tw.Close();
-            }
-            else if (File.Exists(endServicePath))
-            {
-                TextWriter tw = new StreamWriter(endServicePath, true);
-                tw.WriteLine("Service ended at " + endServiceDateAndTime);
-                tw.Close();
+                if (!File.Exists(filePath))
+                {
+                    File.Create(filePath);
+                    endWriter.WriteLine("Service Ended At: " + endTime);
+                }
+                else if (File.Exists(filePath))
+                {
+                    endWriter.WriteLine("Service Ended At: " + endTime);
+                }
             }
         }
     }
